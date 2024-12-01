367653deb7a91b4a0d66cf1c4d2bc24d6a67dc16
Peter Mullins <jc310xc@gmail.com>
2024-12-01 02:43:11 -0500

    prep code!

---
+++ prep/hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+C:\Users\peter\Projects\advent-of-code\aoc-2024\prep\hello-rust\target\debug\deps\hello_rust.exe: src/main.rs
+
+C:\Users\peter\Projects\advent-of-code\aoc-2024\prep\hello-rust\target\debug\deps\hello_rust.d: src/main.rs
+
+src/main.rs:
---
+++ prep/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+C:\Users\peter\Projects\advent-of-code\aoc-2024\prep\hello-rust\target\debug\hello-rust.exe: C:\Users\peter\Projects\advent-of-code\aoc-2024\prep\hello-rust\src\main.rs
