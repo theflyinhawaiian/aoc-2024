1c1299664fcd8bcbd527fb3a981fdbadd2f4a711
Peter Mullins <jc310xc@gmail.com>
2024-12-03 01:41:38 -0500

    simplify prep project to be a template to pull data

--- prep/hello-rust/src/main.rs
+++ prep/hello-rust/src/main.rs
@@ -2,13 +2,8 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let file_path = &args[1];
-    println!("In file {file_path}");
-
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string("data.txt")
         .expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
+    println!("{}", contents);
 }
