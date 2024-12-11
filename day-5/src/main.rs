9b0dc5062ec98abd875ad902ca62577f181067d5
Peter Mullins <jc310xc@gmail.com>
2024-12-11 03:08:44 -0500

    day 5 part 2 wip

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -19,11 +19,15 @@ fn main() {
              .map(|list| list.split(",").map(|x| x.parse::<usize>().ok().unwrap()).collect::<Vec<usize>>()).collect();
 
     let result : usize = lists.iter().filter_map(|list| {
-        let mut valid = true;
         let mut banned_nums : Vec<usize> = Vec::new();
+        let mut result = 0;
         for i in 0..list.len()-1 {
             if banned_nums.contains(&list[i]) {
-                valid = false;
+                // construct valid list from here
+                let valid_list = cons(&list, &rules);
+                println!("original list: {}\nnew list: {}", list.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(","), 
+                valid_list.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(","));
+                result = valid_list[valid_list.len()/2];
                 break;
             }
 
@@ -31,8 +35,38 @@ fn main() {
                 banned_nums.extend(next_bans);
             }
         }
-        if valid { Some(list[list.len()/2]) } else { None }
+
+        if result > 0 { Some(result) } else { None }
     }).sum();
 
     println!("{}", result);
+}
+
+fn cons(original_list: &Vec<usize>, rules : &HashMap<usize, Vec<usize>>) -> Vec<usize>{
+    let mut copied = original_list.clone();
+
+    (0..original_list.len()-1).map(|_| {
+        println!("remaining list: {}", copied.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(","));
+        get_last(&mut copied, rules)
+    }).collect()
+}
+
+fn get_last(list : &mut Vec<usize>, rules : &HashMap<usize, Vec<usize>>) -> usize {
+    for i in 0..list.len()-1 {
+        if comes_last(list[i], list, rules) {
+            return list.remove(i);
+        }
+    }
+    
+    panic!("get_last really should return something")
+}
+
+fn comes_last(x : usize, list : &Vec<usize>, rules: &HashMap<usize, Vec<usize>>) -> bool {
+    list.iter().all(|i| {
+        if let Some(rule) = rules.get(i) {
+            !rule.contains(&x)
+        } else {
+            false
+        }
+    })
 }
