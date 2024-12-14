54897b2c6f2eaf7ba987c649d1a32842839c4efa
Pete Mullins <jc310xc@gmail.com>
2024-12-14 00:53:28 -0500

    day 5 part 2

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     let result : usize = lists.iter().filter_map(|list| {
         let mut banned_nums : Vec<usize> = Vec::new();
         let mut result = 0;
-        for i in 0..list.len()-1 {
+        for i in 0..list.len() {
             if banned_nums.contains(&list[i]) {
                 // construct valid list from here
                 let valid_list = cons(&list, &rules);
@@ -44,17 +44,19 @@ fn main() {
 
 fn cons(original_list: &Vec<usize>, rules : &HashMap<usize, Vec<usize>>) -> Vec<usize>{
     let mut copied = original_list.clone();
-
-    (0..original_list.len()-1).map(|_| {
-        println!("remaining list: {}", copied.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(","));
-        get_last(&mut copied, rules)
-    }).collect()
+    (0..original_list.len()).fold(Vec::new(),|curr, _| {
+        build(&curr, &mut copied, rules)
+    })
 }
 
-fn get_last(list : &mut Vec<usize>, rules : &HashMap<usize, Vec<usize>>) -> usize {
-    for i in 0..list.len()-1 {
-        if comes_last(list[i], list, rules) {
-            return list.remove(i);
+fn build(new_list: &Vec<usize>, list : &mut Vec<usize>, rules : &HashMap<usize, Vec<usize>>) -> Vec<usize> {
+    let mut diff = list.clone();
+    diff.retain(|x| !new_list.contains(x));
+    let mut updated = new_list.clone();
+    for i in 0..diff.len() {
+        if comes_last(diff[i], &diff, rules) {
+            updated.push(diff[i]);
+            return updated
         }
     }
     
@@ -66,7 +68,7 @@ fn comes_last(x : usize, list : &Vec<usize>, rules: &HashMap<usize, Vec<usize>>)
         if let Some(rule) = rules.get(i) {
             !rule.contains(&x)
         } else {
-            false
+            true
         }
     })
 }
