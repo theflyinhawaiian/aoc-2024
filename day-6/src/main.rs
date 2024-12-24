2a8d234c19692ab97dcbf08ade4acef2d04b04d0
Peter Mullins <jc310xc@gmail.com>
2024-12-24 16:12:12 -0500

    day 6 wip

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,8 +1,41 @@
 use std::fs;
+use itertools::Itertools;
 
 fn main() {
     let contents = fs::read_to_string("data.txt")
         .expect("Should have been able to read the file");
 
-    println!("{}", contents.lines().collect::<Vec<&str>>().join("\n"));
+    let chars : Vec<Vec<char>> = contents.lines().map(|x| x.chars().collect()).collect();
+
+    let starting_pos = cols(cols(cols(chars)));
+
+    let (x, y) = get_starting_pos(&starting_pos);
+
+    println!("{}, {}", x, y);
+    
+    print(&starting_pos);
+}
+
+fn print(lines: &Vec<Vec<char>>){
+    println!("{}", lines.clone().iter().map(|x| { x.iter().join(" ") }).join("\n"));
+}
+
+fn cols(lines: Vec<Vec<char>>) -> Vec<Vec<char>>{
+    (0..lines.len()).map(|row| {
+        (0..lines[lines.len()-1-row].len()).map(|col| {
+            lines[col][lines.len()-1-row]
+        }).collect::<Vec<char>>()
+    }).collect::<Vec<Vec<char>>>()
+}
+
+fn get_starting_pos(lines: &Vec<Vec<char>>) -> (usize, usize) {
+    for i in 0..lines.len(){
+        for j in 0..lines[i].len(){
+            if lines[i][j] == '^' {
+                return (i, j)
+            }
+        }
+    }
+
+    (lines.len(), lines.len())
 }
